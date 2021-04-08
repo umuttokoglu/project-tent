fa1ff2ff8f1b5955553a848546ddb168baafd6a3
umuttokoglu <umuttokoglu@gmail.com>
2021-04-08 23:38:29 +0300

    migrations filled

---
+++ app/Constants/MigrationConstants.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Constants;
+
+class MigrationConstants
+{
+    //global
+    public const DEFAULT_STRING_LENGTH = 255;
+    public const DEFAULT_FILE_EXTENSION_LENGTH = 5;
+
+    //category_files
+    public const ENUM_PDF = 'PDF';
+    public const ENUM_IMG = 'IMG';
+}
